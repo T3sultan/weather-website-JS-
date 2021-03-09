b37a2be5ef8095dd4e7f5283c6058e4693fe63b2
T3sultan <incognito.soul011@gmail.com>
2021-03-10 04:19:36 +0600

    generate date

--- main.js
+++ main.js
@@ -8,21 +8,21 @@ class Weather {
     }
     async getWeather() {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.appid}&units=metric`)
-            .then(data => data.json())
+           const responseData=await response.json();
         return {
-            main_data: response.main,
-            overall_data: response.weather[0],
-            sys_data: response.sys
-        }
+            main_data: responseData.main,
+            overall_data: responseData.weather[0],
+            cityName:responseData.name
+            
+        };
     }
-    // setLocation(city, country) {
-    //     this.city = city;
-    //     this.country = country;
-    // }
+    changeLocation(city, country) {
+        this.city = city;
+        this.country = country;
+    }
 
 }
-//instantiate weathe class
-const weather = new Weather('Rongpur', 'BD');
+
 class UI {
     constructor() {
         this.icon = document.getElementById('w-icon');
@@ -30,25 +30,57 @@ class UI {
         this.temp = document.getElementById('temp');
         this.pressure = document.getElementById('pressure');
         this.humidity = document.getElementById('humidity');
+        this.city = document.getElementById('w-city');
+       
     }
-    paint({ main_data:{temp,pressure,humidity}, overall_data:{main,icon}}) {
+    paint({
+         
+        main_data:{temp,pressure,humidity},
+        overall_data:{main,icon},
+        cityName
+    }) {
         const iconUrl = UI.generateIcon(icon);
-        this.icon.setAttribute('src',iconUrl)
+        this.icon.setAttribute('src',iconUrl);
         this.feels.textContent =main;
         this.temp.textContent = `Temperature(cel) ${temp}`;
         this.pressure.textContent =`ressure(hpa) ${pressure}`;
         this.humidity.textContent = `Humidity(%) ${humidity}`;
+        this.city.textContent=cityName;
+       
     }
     static generateIcon(icon){
         return  "http://openweathermap.org/img/w/" + icon + ".png";
     }
+    clearField(){
+        document.getElementById('city').value =' ';
+        document.getElementById('country').value =' ';
+    }
 
 }
 
-const ui = new UI('London', 'UK');
+//instantiate weathe class
+const weather = new Weather('Comilla', 'BD');
 
-weather.getWeather().then(data => {
-    console.log(data);
-    ui.paint(data)
+
+document.getElementById('w-form').addEventListener('submit',e=>{
+    e.preventDefault();
+  const city = document.getElementById('city').value;
+  const country = document.getElementById('country').value;
+  if (city=== ' ' || country ===' ') {
+      console.log("Please provid a information")
+  }else{
+      weather.changeLocation(city, country);
+      ui.clearField()
+      weatherData();
+  }
+ 
 })
+const ui = new UI();
 
+function weatherData(){
+    weather.getWeather().then(data => {
+        console.log(data);
+        ui.paint(data);
+    });
+
+}
